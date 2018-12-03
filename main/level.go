a3fe07f9b14f29c14b95ac244460e842e5933878
net7ton <net7ton@gmail.com>
2018-12-03 17:50:08 +0300

    tombstones on places of sheep die

--- main/level.go
+++ main/level.go
@@ -69,3 +69,21 @@ embedded_components {
     w: 1.0
   }
 }
+embedded_components {
+  id: "ftombs"
+  type: "factory"
+  data: "prototype: \"/main/tombstone.go\"\n"
+  "load_dynamically: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
