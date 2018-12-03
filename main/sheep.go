77d0def15f32450033206d94f807208404e6c555
net7ton <net7ton@gmail.com>
2018-12-03 16:50:39 +0300

    added sounds

--- main/sheep.go
+++ main/sheep.go
@@ -53,23 +53,3 @@ embedded_components {
     w: 1.0
   }
 }
-embedded_components {
-  id: "sound_over"
-  type: "sound"
-  data: "sound: \"/sound/gover.wav\"\n"
-  "looping: 0\n"
-  "group: \"master\"\n"
-  "gain: 1.0\n"
-  ""
-  position {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
-}
