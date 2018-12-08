591388b8fb432532b08224fdc8aba1ef13071f54
net7ton <net7ton@gmail.com>
2018-12-08 22:15:17 +0300

    added sheeps shadows

---
+++ main/shadow.go
@@ -0,0 +1,20 @@
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "tile_set: \"/images/main.atlas\"\n"
+  "default_animation: \"sheep_shadow\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 1.0
+    y: -3.0
+    z: -0.05
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
