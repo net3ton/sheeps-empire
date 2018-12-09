1177ffb7a08f4861927af0a789cd2eafe01d0b48
net7ton <net7ton@gmail.com>
2018-12-09 19:46:13 +0300

    nothing

--- main/sheeps.lua
+++ main/sheeps.lua
@@ -42,57 +42,6 @@ function Sheeps.update(dt)
 	end
 end
 
---[[
-function Sheeps.explosion(sheepId)
-	local sheepPos = go.get_position(sheepId)
-	local sheedInd = nil
-
-	for ind, sid in pairs(Sheeps.units) do
-		if sid ~= sheepId then
-			local spos = go.get_position(sid)
-			local dist = spos - sheepPos
-
-			local length = vmath.length(dist)
-			local vel = dist * (20000.0 / (length * length))
-			go.set(msg.url("main", sid, "script"), "explo", vel)
-		else
-			sheedInd = ind
-		end
-	end
-
-	table.remove(Sheeps.units, sheedInd)
-	go.delete(sheepId)
-end
-]]
-
---[[
-function Sheeps.explo(ind)
-	local sheepId = Sheeps.units[ind]
-	local sheepPos = go.get_position(sheepId)
-
-	for _, sid in pairs(Sheeps.units) do
-		if sid ~= sheepId then
-			local spos = go.get_position(sid)
-			local dist = spos - sheepPos
-
-			--local vel = go.get(msg.url("main", sid, "script"), "velocity")
-			--vel = vel + dist * 100.0
-			--go.set(msg.url("main", sid, "script"), "velocity", vel)
-
-			local length = vmath.length(dist)
-			local vel = dist * (20000.0 / (length * length))
-			go.set(msg.url("main", sid, "script"), "explo", vel)
-		end
-	end
-
-	table.remove(Sheeps.units, ind)
-	go.delete(sheepId)
-
-	sheepId = Sheeps.units[ind]
-	msg.post(sheepId, "black")
-end
-]]
-
 function Sheeps.findNearest(to_pos)
 	local min = -1
 	local sheepId = nil
